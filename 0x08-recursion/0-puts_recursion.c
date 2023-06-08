72432b89692ded546f2d3e4ff1ee0c21bb6b5f32
lilian017 <lilian.gitau211@gmail.com>
2023-06-08 00:58:29 -0700

    Task 0'

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *_puts_recursion - function that prints a string
+ *@s: string pointer
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
+}

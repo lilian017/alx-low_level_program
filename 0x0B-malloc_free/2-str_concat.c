18572cd4f56a533a0d2ff953e4a742cae8eab68d
lilian017 <lilian.gitau211@gmail.com>
2023-06-13 04:02:39 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,10 +24,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s2 != NULL)
 	{
-		j = 0;
+		i = 0;
 		while (s2[i++] != '\0')
 			ln2++;
 	}
+
 	ln = ln1 + ln2;
 	result = (char *)malloc(sizeof(char) * (ln + 1));
 	if (result == NULL)
@@ -35,8 +36,8 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; i < ln1; i++)
 		result[i] = s1[i];
-	for (j = 0; j < ln2; j++, j++)
-		result[j] = s2[j];
+	for (j = 0; j < ln2; j++, i++)
+		result[i] = s2[j];
 	result[ln] = '\0';
 
 	return (result);

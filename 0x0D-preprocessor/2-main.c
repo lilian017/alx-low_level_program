f57fe7cb805a106257d8c67e425d8cc2794cf4e3
lilian017 <lilian.gitau211@gmail.com>
2023-06-19 05:41:56 -0700

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - function that prints name it was compiled from
+ *
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

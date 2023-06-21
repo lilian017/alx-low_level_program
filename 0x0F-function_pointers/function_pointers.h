bb669d39bf7d5cc28ac0feb998de837953d65c71
lilian017 <lilian.gitau211@gmail.com>
2023-06-21 06:08:21 -0700

    Task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,8 @@
-#ifndef function_pointers.h
-#define function_pointers.h
+#ifndef function_pointers_h
+#define function_pointers_h
 
-int _puchar.c
+int _puchar(int c);
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
 

b3cbe2a9aaeb0547607b2fd1230989f0deaf4dc5
lilian017 <lilian.gitau211@gmail.com>
2023-07-04 15:54:02 -0700

    Task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -19,7 +19,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (ptr == NULL)
 		return (NULL);
 	ptr->n = n;
-	ptr-> next = *head;
+	ptr->next = *head;
 	*head = ptr;
 	return (ptr);
 }

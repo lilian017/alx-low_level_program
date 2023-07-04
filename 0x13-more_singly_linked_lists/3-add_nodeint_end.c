f2c36b177f643f231a2ecec0cb73755f6787f164
lilian017 <lilian.gitau211@gmail.com>
2023-07-04 15:58:54 -0700

    Task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,13 +12,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *addednode;
 	listint_t *temp;
-	
+
 	if (head == NULL)
 		return (NULL);
 
 	addednode = malloc(sizeof(listint_t));
 	if (addednode == NULL)
-		return NULL;
+		return (NULL);
 	addednode->n = n;
 	addednode->next = NULL;
 	if (*head == NULL)
@@ -28,9 +28,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	}
 	temp = *head;
 
-		while(temp->next != NULL)
+		while (temp->next != NULL)
 		{
-			temp = temp-> next;
+			temp = temp->next;
 		}
 		temp->next = addednode;
 	return (addednode);

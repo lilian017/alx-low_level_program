c567bc6140a3a3012c879b99191ac59d0acfdc84
lilian017 <lilian.gitau211@gmail.com>
2023-07-10 05:12:56 -0700

    Updated task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (length = 0; text_content[length];)
-			length ++;
+			length++;
 	}
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, length);
